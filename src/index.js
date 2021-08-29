f6fed90cde40132a6b79fd63a37209add092f3ed
rachelbell27 <86980993+rachelbell27@users.noreply.github.com>
2021-08-28 21:00:11 -0400

    added js forecast & removed temp switch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let key = "d2e2c79fc9c89249abd1e2c823668949";
-let units = "metric";
+let units = "imperial";
 let apiRoot = "https://api.openweathermap.org/data/2.5/weather?";
+let ocApiRoot = "https://api.openweathermap.org/data/2.5/onecall?";
 let displayTemp = document.querySelector("#temperature-element");
 let displayCity = document.querySelector("#city-element");
 let displayCountry = document.querySelector("#country-element");
@@ -10,9 +11,9 @@ let displayHumidity = document.querySelector("#humidity-element");
 let displayWind = document.querySelector("#wind-element");
 let searchForm = document.querySelector("#search-form");
 let displayImage = document.querySelector("#weather-icon");
-let celsiusSelector = document.querySelector("#celsius-temperature");
-let fahrenheitSelector = document.getElementById("fahrenheit-temperature");
-let celsiusTemp = 13;
+let temp = 13;
+let lat = 40.7143;
+let lon = -74.006;
 
 function formatDate(now) {
   let days = [
@@ -35,23 +36,52 @@ function formatDate(now) {
   }
   return `${weekDay}, ${hour}:${minute}`;
 }
-function calculateFahrenheit(event) {
-  event.preventDefault();
-  celsiusSelector.classList.remove("active");
-  fahrenheitSelector.classList.add("active");
-  let fahrenheitTemp = Math.round((celsiusTemp * 9) / 5 + 32);
-  displayTemp.innerHTML = fahrenheitTemp;
+
+function handleForecast(response) {
+  let dayResponse = response.data.daily;
+  let displayForecast = document.querySelector("#forecast");
+  let forecastHTML = `<hr /> <div class="row">`;
+
+  dayResponse.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `          
+        <div class="col-2">
+            <div class="forecast-date">${forecastDay.dt}</div>
+            <img
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt=""
+              width="42"
+            />
+            <div>
+              <span class="forecast-temp-max"> ${Math.round(
+                forecastDay.temp.max
+              )}° </span>
+              <span class="forecast-temp-min"> ${Math.round(
+                forecastDay.temp.min
+              )}° </span>
+            </div>
+          </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  console.log(forecastHTML);
+  displayForecast.innerHTML = forecastHTML;
 }
 
-function displayCelsius(event) {
-  event.preventDefault();
-  fahrenheitSelector.classList.remove("active");
-  celsiusSelector.classList.add("active");
-  displayTemp.innerHTML = celsiusTemp;
+function getForecast() {
+  let forecastApiUrl = `${ocApiRoot}units=${units}&lat=${lat}&lon=${lon}&appid=${key}`;
+  axios.get(forecastApiUrl).then(handleForecast);
 }
 
 function handleWeather(response) {
-  celsiusTemp = Math.round(response.data.main.temp);
+  console.log(response);
+  temp = Math.round(response.data.main.temp);
+  lat = response.data.coord.lat;
+  lon = response.data.coord.lon;
   let cityName = response.data.name;
   let countryName = response.data.sys.country;
   let humidity = response.data.main.humidity;
@@ -59,15 +89,15 @@ function handleWeather(response) {
   let imageResponse = response.data.weather[0].icon;
   let skyResponse = response.data.weather[0].description;
   let imgURL = `https://openweathermap.org/img/wn/${imageResponse}@2x.png`;
-  celsiusSelector.click();
   displayDate.innerHTML = formatDate(new Date());
-  displayTemp.innerHTML = celsiusTemp;
+  displayTemp.innerHTML = temp;
   displayCity.innerHTML = cityName;
   displayCountry.innerHTML = countryName;
   displayHumidity.innerHTML = humidity;
   displayWind.innerHTML = windSpeed;
   displayImage.setAttribute("src", imgURL);
   displaySky.innerHTML = skyResponse;
+  getForecast();
 }
 
 function handleSearch(event) {
@@ -84,5 +114,3 @@ function getWeather(city) {
 
 getWeather("New York");
 searchForm.addEventListener("submit", handleSearch);
-fahrenheitSelector.addEventListener("click", calculateFahrenheit);
-celsiusSelector.addEventListener("click", displayCelsius);
